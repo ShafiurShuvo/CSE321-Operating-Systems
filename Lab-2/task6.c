a815a6c0c779122a25d3ce300269c1fb82c6c1b0
Shafiur Rahman Shuvo <shafiurshuvo329@gmail.com>
2025-08-28 19:48:49 +0600

    cp

---
+++ Lab-2/task6.c
@@ -0,0 +1,13 @@
+// While Loop
+
+#include <stdio.h>
+
+int main(void) {
+    int var = 1;
+    
+    while (var <= 5) {
+        printf("%d\n", var);
+        var++;
+    }
+    return 0;
+}
---
+++ Lab-2/task9.c
@@ -0,0 +1,9 @@
+//String
+
+#include <stdio.h>
+
+int main(void) {
+      char name[4] = "John";
+      // char name[4] = "{'J', 'o', 'h', 'n'}";
+      printf("Name: %s\n", name);    
+}
