3a10293b4d6a5265a465e85ed35e5e2c62ad41d1
Shafiur Rahman Shuvo <shafiurshuvo329@gmail.com>
2025-10-27 18:26:18 +0600

    Lab task

--- Lab-4/labtask.c
+++ Lab-4/labtask.c
@@ -1,36 +1,58 @@
+// Write a C program that has three processes (Parent, Child, and Grandchild.)
+// Parent --> Prints the even values of a given array
+// Child --> Prints the odd values of a given array	
+// Grandchild --> Prints the length of a given array
+// Execution order -->
+//        Parent --> Grandchild --> Child
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 
 int main() {
-	int even [4] ;
-	int odd [4] ;
-	int s [] = {1,2,3,4,5,6,7,8};
-	int len = sizeof(s)/sizeof(s[0]);
-	for (int i = 0; i<len; i++){
-		if (s[i]%2 ==0) {
-			even = s[i];
-			printf("%d\n", s[i]);
-			}
-		else {
-			odd = s[i];
-			printf("%d\n", s[i]);
-		}
-	}
-	
-	pid_t pid1, pid2;
-	pid1 = fork();
-	if (pid1 > 0){
-	printf("even\n");
-	}
-	else if (pid1 == 0) {
-	wait(NULL);
-	printf("odd\n");
-	pid2 = fork();
-		
-		if (pid2 == 0){
-		printf("length is %d\n", len);
-		}
-	}
+    int s[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int len = sizeof(s) / sizeof(s[0]);
+
+    pid_t pid1, pid2;
+
+    pid1 = fork();
+
+    if (pid1 < 0) {
+        perror("Error in fork");
+        return 1;
+    }
+
+    if (pid1 > 0) {  // Parent
+        printf("Even numbers:\n");
+        for (int i = 0; i < len; i++) {
+            if (s[i] % 2 == 0)
+                printf("%d ", s[i]);
+        }
+        printf("\n");
+        wait(NULL);
+    } 
+    else {
+        pid2 = fork();
+
+        if (pid2 < 0) {
+            perror("Error in fork");
+            return 1;
+        }
+
+        if (pid2 == 0) {  // Grandchild
+            printf("Array length: %d\n", len);
+        } 
+        else {  // Child
+            wait(NULL);
+            printf("Odd numbers:\n");
+            for (int i = 0; i < len; i++) {
+                if (s[i] % 2 != 0)
+                    printf("%d ", s[i]);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
 }
